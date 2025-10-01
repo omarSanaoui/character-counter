03fa48975894fe20c19fafd99d0dbe3e9f25e580
Omar Sanaoui <omarsanaoui5@gmail.com>
2025-10-01 08:48:35 +0100

    adding mode toggler

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 //toggle dark-light mode
-const theme_switcher = document.querySelector("switch-themes");
-const logo = document.querySelector(".logo")
+const theme_switcher = document.querySelector(".switch-themes");
+const logo = document.querySelector(".logo");
 const body = document.querySelector("body");
 
-let mode = "dark-mode";
+let mode = localStorage.getItem("theme") || "dark-mode";
+body.classList.remove("dark-mode", "light-mode");
+body.classList.add(mode);
+updateIcons(mode);
 
 theme_switcher.addEventListener("click",() => {
-    body.classList.toggle("dark-mode");
+    mode = mode === "dark-mode" ? "light-mode" : "dark-mode";
+    body.classList.remove("dark-mode", "light-mode");
+    body.classList.add(mode);
+    localStorage.setItem("theme",mode);
+    localStorage.getItem("theme")
+
+    updateIcons(mode);
 })
+
+function updateIcons(mode){
+    if(mode == "dark-mode"){
+        logo.src = "icons/logo-dark-theme.svg";
+        theme_switcher.src = "icons/icon-sun.svg";
+    }else{
+        logo.src = "icons/logo-light-theme.svg";
+        theme_switcher.src = "icons/icon-moon.svg";
+    }
+}
+//counting
+
